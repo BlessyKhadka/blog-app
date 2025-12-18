ae909d9eb16ea2c66e5807a4405d96a491e5d2b5
Blessy Khadka <blessykhadka@Blessys-MacBook-Pro-4.local>
2025-12-17 21:50:46 -0500

    Completed Newspaper app with CRUD functionality

--- django_project/settings.py
+++ django_project/settings.py
@@ -51,8 +51,21 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # Third-party
+    "crispy_forms",
+    "crispy_bootstrap5",
+
+    # Local apps
+    "accounts",
+    "pages",
     "blog",
+    "articles",
 ]
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+TIME_ZONE = "America/New_York"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- django_project/urls.py
+++ django_project/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blog.urls")),
+    path("", include("pages.urls")),
+    path("articles/", include("articles.urls")),  # 👈 Newspaper app
 ]
+
